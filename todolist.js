32150c9af97f7e7b6d7779c5f392e37e83bb7cc8
ajewe <ajewe@utexas.edu>
2020-01-06 19:44:45 -0600

    added html elements

--- todolist.js
+++ todolist.js
@@ -62,3 +62,13 @@ var todoList = {
   }
 };
 
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();
+});
